d71b978ababbad78563df19110f075e2e1d4dbbe
Brandon Lai <brandon.lai@vanderbilt.edu>
2020-12-27 13:29:09 -0600

    adds getting recipes from api

---
+++ getRecipes.py
@@ -0,0 +1,53 @@
+import requests
+from requests.api import head
+
+def getRecipesFromApi(availableIngredients, numRecipes):
+    url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients'
+
+    params = {'number': str(numRecipes), 'ranking': '1', 'ignorePantry': 'true', 'ingredients': availableIngredients}
+
+    headers = {
+        'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+        'x-rapidapi-key': 'b7c4f968f6mshac8de0e5ac09817p186b7djsn7baceba10779'
+    }
+
+    res = requests.request('GET', url, headers=headers, params=params)
+
+    data = res.json()
+    # print(data)
+
+    # Printing response to console
+    print
+    print ('{')
+
+    for i in range(numRecipes):
+        print(data[i]['title'] + ':')
+        print('Missing Ingredients:')
+
+        for item in data[i]['missedIngredients']:
+            print(item['name'] + ': ' + item['aisle'])
+        
+    print('}')
+
+def main():
+    ingredients = input('Enter the ingredients in your fridge (each separated by a space): ')
+
+    numRecipes = int(input('How many recipes do you want to see: '))
+
+    while numRecipes < 1:
+        numRecipes = int(input('Please enter a number greater than 1: '))
+
+    temp = ingredients.split() # separate the individual ingredients into a list
+
+    # creating a string of ingredients separated by a comma
+    ingredients = ''
+    for item in temp:
+        ingredients = ingredients + item + ', '
+
+    if len(ingredients) != 0:
+        ingredients = ingredients[:-2] # remove the last space and comma
+    
+    getRecipesFromApi(ingredients, numRecipes)
+
+if __name__ == '__main__':
+    main()
